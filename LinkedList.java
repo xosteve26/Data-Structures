4fd703337bdd5fe98dd1a5d80f513f3b5a244e06
Stephen George <73709251+xosteve26@users.noreply.github.com>
2022-04-02 17:03:39 +0000

    Linked List Implementation

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,7 @@ public class LinkedList{
   
   private class Node{
     private int value;
-    private int next;
+    private Node next;
     public Node(int val){
       this.value=val;
       this.next=null;
@@ -11,17 +11,94 @@ public class LinkedList{
   
   private Node head;
   private Node tail;
+  private int count=0;
 
   public void addLast(int item){
     Node node=new Node(item);
     
-    if (head == null){
-      head=tail=node;
+    if (isEmpty()){
+      head=tail=node;     
     }
     else{
       tail.next=node;
       tail=node;
     }
+    count++;
     
+  }
+
+  public void addFirst(int item){
+    Node node=new Node(item);
+    if(isEmpty()){
+      head=tail=node;
+    }
+    else{
+      node.next=head;
+      head=node;
+    }
+    count++;
+  }
+
+  public int indexOf(int item){
+    int count=0;
+    Node current = head;
+    while (current !=null){
+      if(current.value == item){
+        return count;
+      }
+      current=current.next;
+      count++;
+    }
+    return -1;
+  }
+
+  public boolean contains(int item){
+    return indexOf(item)!=-1;
+  }
+
+  public void deleteFirst(){
+    if(isEmpty()){
+      System.out.println("LinkedList is empty!");
+      return; 
+    }
+    if(head == tail){
+      head=tail=null;
+      count--;
+      return;
+    }
+    Node secondNode=head.next;
+    head=null;
+    head=secondNode;
+    count--;
+  }
+
+  public void deleteLast(){
+    Node current=head;
+    if(isEmpty()){
+      System.out.println("LinkedList is empty!");
+      return; 
+    }
+    if(head==tail){
+      head=tail=null;
+      count--;
+      return;
+    }
+  
+    while (current.next!=tail){
+      current=current.next;
+      
+    }
+    Node lastNode=current;
+    lastNode.next=null;
+    tail=lastNode;
+    count--;
+  }
+
+  public int size(){
+    return count;
+  }
+
+  private boolean isEmpty(){
+    return head==null;
   }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 class Main {
   public static void main(String[] args) {
     LinkedList list=new LinkedList();
-    list.addLast(3);
+    
+    // list.addFirst(44);
+    // list.addLast(3);
+    // list.addLast(56);
+    // list.deleteFirst();
+    // list.deleteLast();
+    System.out.println(list.indexOf(3));
+    System.out.println(list.size());
+    
+    
   }
 }
